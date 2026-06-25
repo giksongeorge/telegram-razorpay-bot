184724a30a669a0104f7903e868939dbff03515d
giksongeorge <giksongeorge@gmail.com>
2026-06-25 22:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,5 +68,9 @@ def remove_user_from_group(telegram_id):
 
 
 # --- Entry point ---
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT
+    app.run(host="0.0.0.0", port=port)
+
